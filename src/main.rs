42f21fddfc5cbee65f3ed6dda1841f987d47f1ea
Antonio Aguilar <antonioaja@protonmail.com>
2023-02-06 17:29:04 -0500

    Optimize hsv conversion

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,9 @@ use std::time::Instant;
 
 fn main() -> Result<()> {
     let ope = image::open("OAKD-0014.png").context("Could not open test_rotate.png")?;
-    
+
     let now = Instant::now();
-    
+
     let color_test = control::tools::ColorArea::new(
         Dimensions {
             width: 60,
@@ -24,11 +24,11 @@ fn main() -> Result<()> {
         },
         Point { x: 1060, y: 420 },
         Range {
-            lower: 90.0,
-            upper: 270.0,
+            lower: 175.0,
+            upper: 265.0,
         },
         Range {
-            lower: 0.10,
+            lower: 0.50,
             upper: 1.0,
         },
         Range {
@@ -41,8 +41,9 @@ fn main() -> Result<()> {
         },
     );
 
+    let some_width = ope.width();
+
     let ope_hsv = ope
-        .clone()
         .into_rgb8()
         .as_bytes()
         .as_rgb()
@@ -50,7 +51,9 @@ fn main() -> Result<()> {
         .map(|x| HSV::from_rgb(*x))
         .collect();
 
-    let color_match_percentage = color_test.check(ope_hsv, ope.width());
+    println!("{} ms", now.elapsed().as_millis());
+
+    let color_match_percentage = color_test.check(ope_hsv, some_width);
 
     println!("{}%", color_match_percentage * 100.0);
 
