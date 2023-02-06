a1b0dbd81c13d801d5e58b5867ee85a8124c0de1
Antonio <antonioaja@protonmail.com>
2023-02-05 22:04:30 -0500

    Reordered tests

--- src/tests/colorspaces_test.rs
+++ src/tests/colorspaces_test.rs
@@ -5,7 +5,7 @@ use rgb::RGB;
 use crate::control::colorspaces::HSV;
 
 #[test]
-fn rgb8_to_hsv64() -> Result<()> {
+fn rgb8_to_hsv_f64() -> Result<()> {
     (0..=u8::MAX).into_par_iter().for_each(|r| {
         (0..=u8::MAX).for_each(|g| {
             (0..=u8::MAX).for_each(|b| {
@@ -23,28 +23,8 @@ fn rgb8_to_hsv64() -> Result<()> {
     Ok(())
 }
 
-// Impossibly long to test
-// #[test]
-// fn rgb16_to_hsv64() -> Result<()> {
-//     (0..=u16::MAX).into_par_iter().for_each(|r| {
-//         (0..=u16::MAX).for_each(|g| {
-//             (0..=u16::MAX).for_each(|b| {
-//                 let original = RGB::new(r, g, b);
-//                 let intermediate: HSV<f64> = HSV::from_rgb::<u16>(original);
-//                 let final_out = HSV::<f64>::to_rgb(intermediate)
-//                     .context("Could not convert from HSV to RGB!")
-//                     .unwrap();
-
-//                 assert_eq!(original, final_out);
-//             });
-//         });
-//     });
-
-//     Ok(())
-// }
-
 #[test]
-fn rgb8_to_hsv32() -> Result<()> {
+fn rgb8_to_hsv_f32() -> Result<()> {
     (0..=u8::MAX).into_par_iter().for_each(|r| {
         (0..=u8::MAX).for_each(|g| {
             (0..=u8::MAX).for_each(|b| {
@@ -71,6 +51,26 @@ fn rgb8_to_hsv32() -> Result<()> {
 //                 let original = RGB::new(r, g, b);
 //                 let intermediate: HSV<f32> = HSV::from_rgb::<u16>(original);
 //                 let final_out = HSV::<f32>::to_rgb(intermediate)
+//                     .context("Could not convert from HSV to RGB!")
+//                     .unwrap();
+
+//                 assert_eq!(original, final_out);
+//             });
+//         });
+//     });
+
+//     Ok(())
+// }
+
+// Impossibly long to test
+// #[test]
+// fn rgb16_to_hsv64() -> Result<()> {
+//     (0..=u16::MAX).into_par_iter().for_each(|r| {
+//         (0..=u16::MAX).for_each(|g| {
+//             (0..=u16::MAX).for_each(|b| {
+//                 let original = RGB::new(r, g, b);
+//                 let intermediate: HSV<f64> = HSV::from_rgb::<u16>(original);
+//                 let final_out = HSV::<f64>::to_rgb(intermediate)
 //                     .context("Could not convert from HSV to RGB!")
 //                     .unwrap();
 
